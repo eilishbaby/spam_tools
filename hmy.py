fb3d528694abba6e45f1f9b0d00db4d9b4419473
eilish baby <eilishinstagram3@gmail.com>
2023-05-27 23:36:51 +0000

    na dem

---
+++ hmy.py
@@ -0,0 +1,17 @@
+def remove_duplicates(file_name):
+    with open(file_name, 'r') as file:
+        emails = file.read().splitlines()
+
+    unique_emails = list(set(emails))
+
+    with open('clean.txt', 'a') as output_file:
+        for email in unique_emails:
+            output_file.write(email + '\n')
+
+    print("Duplicate removal complete. Clean emails appended to clean.txt")
+
+
+# Accept the file name from the user
+file_name = input("Enter the name of the file: ")
+
+remove_duplicates(file_name)
